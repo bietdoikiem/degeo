110e0bdacd44793b1ca00811baafc2c9fa7988c1
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-09-04 14:42:02 +0700

    add websocket and chat code

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ var videoRouter = require('./routes/Video');
 var roomRouter = require('./routes/Room');
 var playlistRouter = require('./routes/playlist');
 var messageRouter = require('./routes/message');
+const cors = require('cors');
 
 var app = express();
 
@@ -22,6 +23,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
