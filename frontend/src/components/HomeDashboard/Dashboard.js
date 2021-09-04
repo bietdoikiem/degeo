6659034a13e1afa0bc31ec1ff3cf2c5a6ab26f25
Minh Pham <pcminh0505@gmail.com>
2021-09-04 17:13:55 +0700

    Done UI dashboard

--- frontend/src/components/HomeDashboard/Dashboard.js
+++ frontend/src/components/HomeDashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Button,
@@ -6,31 +6,60 @@ import {
   Drawer,
   DrawerBody,
   DrawerHeader,
-  DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
 } from "@chakra-ui/react";
 
 import LoginForm from "./LoginForm";
+import RoomData from "./RoomData";
+import RegisterForm from "./RegisterForm";
 
 function Dashboard() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [register, setRegister] = useState(false);
+
+  const onRegister = () => {
+    setRegister(!register);
+  };
+
   return (
     <>
       <Button onClick={onOpen}>Open</Button>
-      <Drawer placement="left" isOpen={isOpen} onClose={onClose} size="md">
-        <DrawerOverlay />
+      <Drawer
+        placement="left"
+        isOpen={isOpen}
+        onClose={onClose}
+        size="md"
+        trapFocus={false}
+        closeOnOverlayClick={false}
+      >
+        {/* <DrawerOverlay /> */}
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>Room Details</DrawerHeader>
 
           <DrawerBody>
-            <LoginForm />
+            <RoomData />
+            {!register ? (
+              <>
+                <LoginForm />{" "}
+                <Button colorScheme="teal" onClick={onRegister}>
+                  Did not have account? Register
+                </Button>
+              </>
+            ) : null}
+            {register ? (
+              <>
+                <RegisterForm />
+                <Button colorScheme="blue" onClick={onRegister}>
+                  Already have account? Sign in
+                </Button>
+              </>
+            ) : null}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
     </>
   );
 }
-
 export default Dashboard;
--- frontend/src/components/HomeDashboard/RegisterForm.js
+++ frontend/src/components/HomeDashboard/RegisterForm.js
@@ -1,18 +1,36 @@
 import React from "react";
+// import axios from "axios";
 import { useFormik } from "formik";
-import { Stack, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Stack,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 import * as Yup from "yup";
 
-function LoginForm() {
+function RegisterForm() {
   const { handleSubmit, handleChange, values, touched, errors, handleBlur } =
     useFormik({
       initialValues: {
         username: "",
         password: "",
+        confirmPassword: "",
       },
       validationSchema: Yup.object({
         username: Yup.string().required("Username can't be empty"),
-        password: Yup.string().required("Password can't be empty"),
+        password: Yup.string("Password can't be empty")
+          .min(8, "Password must be at least 8 characters")
+          .required(),
+        confirmPassword: Yup.string().when("password", {
+          is: (val) => !!(val && val.length > 0),
+          then: Yup.string().oneOf(
+            [Yup.ref("password")],
+            "Both password need to be the same"
+          ),
+        }),
       }),
       onSubmit: (v) => {
         alert(JSON.stringify(v, null, 2));
@@ -33,6 +51,9 @@ function LoginForm() {
               onBlur={handleBlur}
             />
           </FormControl>
+          {touched.username && errors.username ? (
+            <Text color="red">{errors.username}</Text>
+          ) : null}
           <FormControl isRequired>
             <FormLabel>Password</FormLabel>
             <Input
@@ -44,11 +65,24 @@ function LoginForm() {
             />
           </FormControl>
           {touched.password && errors.password ? (
-            <div>{errors.password}</div>
+            <Text color="red">{errors.password}</Text>
+          ) : null}
+          <FormControl isRequired>
+            <FormLabel>Confirm Password</FormLabel>
+            <Input
+              type="password"
+              id="confirmPassword"
+              value={values.confirmPassword}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </FormControl>
+          {touched.confirmPassword && errors.confirmPassword ? (
+            <Text color="red">{errors.confirmPassword}</Text>
           ) : null}
           <FormControl>
-            <Button colorScheme="blue" type="submit">
-              Log in to join the room!
+            <Button colorScheme="green" type="submit">
+              Register
             </Button>
           </FormControl>
         </Stack>
@@ -57,4 +91,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default RegisterForm;
